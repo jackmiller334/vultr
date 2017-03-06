a231a9ed2c677a1cf75ee5f761ec7200698c655e
jackmiller334 <jackmiller334@gmail.com>
2017-03-06 12:32:25 +0000

    List application api call

---
+++ lib/applications.go
@@ -0,0 +1,26 @@
+package lib
+
+import "fmt"
+
+// Application on Vultr
+type Application struct {
+    ID int `json:"APPID,string"`
+    Name string `json:"name"`
+    ShortName string `json:"short_name"`
+    DeployName string `json:"deploy_name"`
+    Surcharge int `json:"surcharge"`
+}
+
+// GetPlans returns a list of all available plans on Vultr account
+func (c *Client) GetApplications() ([]Application, error) {
+	var applicationMap map[string]Application
+	if err := c.get(`app/list`, &applicationMap); err != nil {
+		return nil, err
+	}
+
+	var applicationList []Application
+	for _, application := range applicationMap {
+		applicationList = append(applicationList, application)
+	}
+	return applicationList, nil
+}
---
+++ lib/applications_test.go
@@ -0,0 +1,3 @@
+package lib
+
+// to do - implement testing
