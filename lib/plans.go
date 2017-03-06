eb1c831bed2409768f791d5166766e69a2dd26b5
jackmiller334 <jackmiller334@gmail.com>
2017-03-06 11:50:51 +0000

    List VC2 plans

--- lib/plans.go
+++ lib/plans.go
@@ -14,9 +14,29 @@ type Plan struct {
 	Regions   []int  `json:"available_locations"`
 }
 
+// Add sort ability
+type SortByID []Plan
+
+func (b SortByID) Len() int           { return len(b) }
+func (b SortByID) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b SortByID) Less(i, j int) bool { return b[i].ID < b[j].ID }
+
 func (c *Client) GetPlans() ([]Plan, error) {
 	var planMap map[string]Plan
 	if err := c.get(`plans/list`, &planMap); err != nil {
+		return nil, err
+	}
+
+	var planList []Plan
+	for _, plan := range planMap {
+		planList = append(planList, plan)
+	}
+	return planList, nil
+}
+
+func (c *Client) GetVC2Plans() ([]Plan, error) {
+	var planMap map[string]Plan
+	if err := c.get(`plans/list_vc2`, &planMap); err != nil {
 		return nil, err
 	}
 
